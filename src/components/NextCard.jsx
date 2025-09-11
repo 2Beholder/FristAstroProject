85436e4c7c13f41d30b178c2df70942496f5f795
huangyaolong <huangyaolong@dekeinfo.com>
2025-09-11 16:13:02 +0800

    feat: vue框架添加和客户端注水的使用

---
+++ src/components/NextCard.jsx
@@ -0,0 +1,8 @@
+export default function NextCard() {
+  return (
+    <div style={{ border: "1px solid #ccc", padding: "10px" }}>
+      <h3>Next/React 组件</h3>
+      <p>这是一个跨框架组件。</p>
+    </div>
+  );
+}
---
+++ src/components/VueButton.vue
@@ -0,0 +1,28 @@
+<template>
+   <button class="btn" @click="count++">Vue 按钮点击：{{ count }}</button>
+</template>
+​
+<script setup>
+import { ref } from "vue";
+const count = ref(0);
+</script>
+
+<style lang="scss">
+.btn {
+  background-color: #ff9776;
+  border: none;
+  border-radius: 5px;
+  color: white;
+  cursor: pointer;
+  font-size: 1rem;
+  padding: 10px 15px;
+  margin-left: 0;
+
+  margin-top: 10px;
+  margin-bottom: 10px;
+
+  &:hover {
+    background-color: darken(#ff9776, 10%);
+  }
+}
+</style>
